73a8a5620c532193312e370279c98f7904a76273
hernandesicaro <icaro.lg6@gmail.com>
2023-05-03 10:01:24 -0300

    First commit test

--- test zip codes.py
+++ test zip codes.py
@@ -1,2 +1,32 @@
 import pandas as pd
 import numpy as np
+
+#Vou usar o pacote do python de ceps do Brasil
+#antes era conhecido como pycep_correios
+#mas atualmente é o brazilcep (instalar com pip + import)
+
+import brazilcep as bc
+
+#além do pacote do cep eu tenho que importar
+#o pacote de georreferenciamento mais usado do python
+#que é o GeoPy (instarlar com pip + import)
+
+from geopy.geocoders import Nominatim
+
+#'from': An alternate form of the import statement allows individual
+# objects from the module to be imported directly into the caller’s
+# symbol table.
+
+address = bc.get_address_from_cep('04038903')
+print(address)
+
+geolocator = Nominatim(user_agent="test_teste")
+location=geolocator.geocode(address)
+
+print(location.latitude, location.longitude)
+
+
+
+
+
+
