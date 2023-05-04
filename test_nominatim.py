1cb859422f2568071c1db96894eed0bdef305b18
hernandesicaro <icaro.lg6@gmail.com>
2023-05-04 09:37:57 -0300

    bulk test 1

--- test_nominatim.py
+++ test_nominatim.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
 
 
 
-geolocator = Nominatim(user_agent="test_teste")
+geolocator = Nominatim(user_agent="tese_teste")
 
+df = pd.DataFrame({'address': ['Rua Escobar Ortiz Vila Nova Conceição São Paulo',
+                               'Rua Emílio Berla Copacabana Rio de Janeiro',
+                               'Rua Madagascar Parque Oratorio Santo André']})
 
-location=geolocator.geocode("Rua Tuim Moema São Paulo")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
+df['location'] = df['address'].apply(geocode)
 
-print(location)
-print(location.latitude, location.longitude)
+df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+
+print(df)
