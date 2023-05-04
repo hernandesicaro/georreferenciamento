fe8879c85d7595bd5cb20676892573a3a866a9e3
hernandesicaro <icaro.lg6@gmail.com>
2023-05-04 14:58:16 -0300

    bulk test 1

--- test_nominatim.py
+++ test_nominatim.py
@@ -2,21 +2,11 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
-
-
+df = pd.read_csv("nom_test_bulk1.csv")
 
 geolocator = Nominatim(user_agent="tese_teste")
-
-df = pd.DataFrame({'address': ['Rua Escobar Ortiz Vila Nova Conceição São Paulo',
-                               'Rua Emílio Berla Copacabana Rio de Janeiro',
-                               'Rua Madagascar Parque Oratorio Santo André']})
-
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
-df['location'] = df['address'].apply(geocode)
-
-df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+df['point'] = (df['address'].apply(geocode)).apply(lambda loc: tuple(loc.point) if loc else None)
 
 print(df)
-
-#now i have to make a dataset of my unmatched addresses
